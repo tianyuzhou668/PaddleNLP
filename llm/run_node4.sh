62450bf0b3493b4572b4cddd49f0b96ed994a3f4
tianyu.zhou <tianyu.zhou@iluvatar.com>
2024-11-19 15:25:10 +0800

    Update ip address.

--- llm/run_node4.sh
+++ llm/run_node4.sh
@@ -4,4 +4,4 @@
 export NCCL_SOCKET_IFNAME=$(ip a | grep '10.31.10' | awk '{print $7}')
 export NCCL_IB_HCA=mlx5_0
 export FLAGS_enable_ixdnn_attn=true
-python3 -u  -m paddle.distributed.launch --ips=10.31.10.55,10.31.10.69,10.31.10.104,10.31.10.12 --hosts=10.31.10.55 --gpus "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15" run_pretrain.py ./llama/pretrain-llama_13b-pp4tp2sd2_stage1.json
+python3 -u  -m paddle.distributed.launch --ips=10.31.10.55,10.31.10.69,10.31.10.210,10.31.10.12 --hosts=10.31.10.55 --gpus "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15" run_pretrain.py ./llama/pretrain-llama_13b-pp4tp2sd2_stage1.json
